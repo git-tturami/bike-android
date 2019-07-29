d8ac4fe4d0620fbc49856e1bce42b1d10d3bdecf
als95 <als950901@gmail.com>
2019-07-29 16:55:31 +0900

    fix bug that duplicate activities and tmap api with mvp

--- app/src/main/java/com/gitturami/bike/view/main/presenter/MainPresenter.kt
+++ app/src/main/java/com/gitturami/bike/view/main/presenter/MainPresenter.kt
@@ -1,12 +1,19 @@
 package com.gitturami.bike.view.main.presenter
 
-import android.widget.LinearLayout
-import com.skt.Tmap.TMapView
+import com.skt.Tmap.TMapPoint
 
 class MainPresenter : MainContact.Presenter {
-    private lateinit var tview: LinearLayout
+    private lateinit var view: MainContact.View
 
-    override fun takeView(view: LinearLayout) {
-        this.tview = view
+    override fun takeView(view: MainContact.View) {
+        this.view = view
+    }
+
+    override fun test() {
+        // TODO: implementing to get point(latitude, longitude). In some case, we need to implement Point class.
+
+        val tMapPointStart = TMapPoint(37.5048935, 126.9573662) // 중앙대학교
+        val tMapPointEnd = TMapPoint(37.5099724, 126.9949061) // 반포한강공원
+        this.view.findPath(tMapPointStart, tMapPointEnd)
     }
 }
