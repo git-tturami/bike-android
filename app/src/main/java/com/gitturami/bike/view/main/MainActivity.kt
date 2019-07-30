d0d6a2a75966232d63c775431f5bf82b920ae9f0
jin <jindl465@naver.com>
2019-07-30 16:33:04 +0900

    Merge branch 'master' into develop

--- app/src/main/java/com/gitturami/bike/view/main/MainActivity.kt
+++ app/src/main/java/com/gitturami/bike/view/main/MainActivity.kt
@@ -4,61 +4,45 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.LinearLayout
 import com.gitturami.bike.R
 import com.gitturami.bike.view.main.presenter.MainContact
 import com.gitturami.bike.view.main.presenter.MainPresenter
 import com.skt.Tmap.TMapData
 import com.skt.Tmap.TMapPoint
-import com.skt.Tmap.TMapPolyLine
 import com.skt.Tmap.TMapView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainContact.View {
     private lateinit var presenter: MainContact.Presenter
-    //private lateinit var tMapView:TMapView
+    private lateinit var tMapView: TMapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val linearLayoutTmap = findViewById(R.id.linearLayoutTmap) as LinearLayout
-        val tMapView = TMapView(this)
+        val linearLayoutTmap = linearLayoutTmap
+        tMapView = TMapView(this)
 
         tMapView.setSKTMapApiKey("3826adf1-c99a-4e69-95c5-3763539465ea")
         linearLayoutTmap.addView(tMapView)
-
         tMapView.setCenterPoint(126.9573662, 37.5048935)
 
-        val tMapPointStart = TMapPoint(37.5048935, 126.9573662) // 중앙대학교
-        val tMapPointEnd = TMapPoint(37.5099724, 126.9949061) // 반포한강공원
-
-        findpath(tMapPointStart,tMapPointEnd)
-
         presenter = MainPresenter()
-        presenter.takeView(linearLayoutTmap)
+        presenter.takeView(this)
+        presenter.test()
     }
 
-//    fun start() {
-//
-//
-//    }
-
-    fun findpath(start:TMapPoint, end:TMapPoint){
+    override fun findPath(start:TMapPoint, end:TMapPoint) {
         try {
             val data = TMapData()
             Log.d("gess","gess")
-            data.findPathData(start, end, object: TMapData.FindPathDataListenerCallback {
-                override fun onFindPathData(path: TMapPolyLine){
-                    runOnUiThread(object: Runnable {
-                        override fun run(){
-                            path.setLineWidth(5f)
-                            path.setLineColor(Color.BLUE)
-                            //tMapView.addTMapPath(path)
-                        }
-                    })
+            data.findPathData(start, end) { path ->
+                runOnUiThread {
+                    path.lineWidth = 5f
+                    path.lineColor = Color.BLUE
+                    tMapView.addTMapPath(path)
                 }
-            })
-
+            }
         } catch (e: Exception) {
             e.printStackTrace()
         }
