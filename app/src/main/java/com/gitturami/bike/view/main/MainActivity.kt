0496fae4b0ff784cac6b037063522e4a3c1debd9
jjiijj78 <jjiijj78@naver.com>
2019-07-30 13:10:34 +0900

    2019-7-30, have nothing meaning

--- app/src/main/java/com/gitturami/bike/view/main/MainActivity.kt
+++ app/src/main/java/com/gitturami/bike/view/main/MainActivity.kt
@@ -5,31 +5,34 @@ import android.graphics.PointF
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.gitturami.bike.R
 
 
 import com.gitturami.bike.view.main.presenter.MainContact
 import com.gitturami.bike.view.main.presenter.MainPresenter
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+
 import com.skt.Tmap.*
 import java.util.ArrayList
 import com.skt.Tmap.TMapMarkerItem
 import com.skt.Tmap.TMapPoint
-
-
-
+import com.google.android.material.bottomsheet.BottomSheetBehavior.*
 
 
 class MainActivity : AppCompatActivity(), MainContact.View{
     private lateinit var presenter: MainContact.Presenter
     private lateinit var tMapView:TMapView
+    private lateinit var bottomSheet:LinearLayout
+
 //    private lateinit var bottomSheet : BottomSheetBehavior<CoordinatorLayout>
     var apiKey = "3826adf1-c99a-4e69-95c5-3763539465ea"
     var mpoint: TMapPoint? = null
-
+    var data = arrayOf("List1","List2","List3","List4","List5","List6","List7","List8","List9","List10","List11","List12")
 
 
 
@@ -41,18 +44,45 @@ class MainActivity : AppCompatActivity(), MainContact.View{
         tMapView = TMapView(this)
         tMapView.setSKTMapApiKey(apiKey)
 
-        val bottomSheet = findViewById(R.id.Bottom_Sheet) as CoordinatorLayout
-        val bottomSheetBehavior = BottomSheetBehavior.from<CoordinatorLayout>(bottomSheet)
+
+        val listView = findViewById<ListView>(R.id.list)
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        listView.adapter = adapter
+
+
+
+        val bottomSheetText:TextView = findViewById(R.id.cat_persistent_bottomsheet_state)
+
+        bottomSheet =  findViewById(com.gitturami.bike.R.id.Bottom_Sheet)
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
 
         bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onSlide(p0: View, slideOffset: Float) {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+//                when (slideOffset) {
+//                    -1F -> bottomSheetBehavior.setState(STATE_EXPANDED)
+//                    0F -> bottomSheetBehavior.setState(STATE_HALF_EXPANDED)
+//                    1F -> bottomSheetBehavior.setState(STATE_COLLAPSED)
+//                    else->{
+//
+//                    }
+//                }
+                    Log.d("TAG", "slideOffset " + slideOffset)
 
-                Log.d("TAG", "slideOffset " + slideOffset)
             }
 
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
 
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    STATE_DRAGGING -> bottomSheetText.setText(R.string.DRAGGING)
+                    STATE_SETTLING -> bottomSheetText.setText(R.string.SETTLING)
+                    STATE_EXPANDED -> bottomSheetText.setText(R.string.EXPANDED)
+                    STATE_COLLAPSED -> bottomSheetText.setText(R.string.COLLAPSED)
+                    STATE_HIDDEN -> bottomSheetText.setText(R.string.HIDDEN)
+                    STATE_HALF_EXPANDED -> bottomSheetText.setText(R.string.HALF_EXPANDED)
+                    else -> {
+                    }
+                }
                 Log.d("TAG", "newState $newState")
             }
         })
