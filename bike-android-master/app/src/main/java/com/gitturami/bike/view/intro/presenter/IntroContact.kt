25e0d6afda9a056c77e76da7b21d9d8a34172672
jin <jindl465@naver.com>
2019-08-10 04:45:56 +0900

    Delete Duplicate Files

--- bike-android-master/app/src/main/java/com/gitturami/bike/view/intro/presenter/IntroContact.kt
+++
@@ -1,5 +0,0 @@
-package com.gitturami.bike.view.intro.presenter
-
-interface IntroContact {
-    fun changeActivity()
-}
--- bike-android-master/app/src/main/java/com/gitturami/bike/view/intro/presenter/IntroPresenter.kt
+++
@@ -1,8 +0,0 @@
-package com.gitturami.bike.view.intro.presenter
-
-class IntroPresenter : IntroContact {
-
-    override fun changeActivity() {
-        //TODO("It will change activity after some time.")
-    }
-}
