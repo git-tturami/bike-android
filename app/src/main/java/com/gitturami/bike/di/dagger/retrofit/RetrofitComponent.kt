1fb04a4d4cc36e7513833472ebc12f78ebd158ec
boohyunsik <bhs9194@nate.com>
2019-09-07 01:42:36 +0900

    feat: retrofit and dagger2

---
+++ app/src/main/java/com/gitturami/bike/di/dagger/retrofit/RetrofitComponent.kt
@@ -0,0 +1,20 @@
+package com.gitturami.bike.di.dagger.retrofit
+
+import android.content.Context
+import com.gitturami.bike.model.station.StationDataManager
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [RetrofitModule::class])
+interface RetrofitComponent {
+    fun inject(stationDataManager: StationDataManager)
+
+    @Component.Builder
+    interface Builder {
+        fun build() : RetrofitComponent
+
+        fun retrofitModule(retrofitModule: RetrofitModule) : Builder
+    }
+}
