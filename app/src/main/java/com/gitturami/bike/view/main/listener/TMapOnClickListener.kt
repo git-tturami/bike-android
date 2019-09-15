2c84419c459bf771192576bd2acdc1948e8a2c78
boohyunsik <bhs9194@nate.com>
2019-09-15 22:31:06 +0900

    impl: default bottom sheet dialog

--- app/src/main/java/com/gitturami/bike/view/main/listener/TMapOnClickListener.kt
+++ app/src/main/java/com/gitturami/bike/view/main/listener/TMapOnClickListener.kt
@@ -2,6 +2,7 @@ package com.gitturami.bike.view.main.listener
 
 import android.graphics.PointF
 import android.widget.LinearLayout
+import com.gitturami.bike.logger.Logger
 import com.gitturami.bike.view.main.presenter.MainContact
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.skt.Tmap.TMapMarkerItem
@@ -14,15 +15,21 @@ import kotlin.math.abs
 class TMapOnClickListener(private var view: MainContact.View, private var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>)
     : TMapView.OnClickListenerCallback {
 
+    companion object {
+        val TAG = "TMapOnClickListener"
+    }
+
     private lateinit var currentClickedMapPoint: TMapPoint
     private lateinit var dragCheckPoint: PointF
 
     override fun onPressEvent(p0: ArrayList<TMapMarkerItem>?, p1: ArrayList<TMapPOIItem>?, p2: TMapPoint?, p3: PointF?): Boolean {
+        Logger.i(TAG, "onPressEvent()")
         dragCheckPoint = p3!!
         return true
     }
 
     override fun onPressUpEvent(p0: ArrayList<TMapMarkerItem>?, p1: ArrayList<TMapPOIItem>?, p2: TMapPoint?, p3: PointF?): Boolean {
+        Logger.i(TAG, "onPressUpEvent()")
         if (!isDrag(dragCheckPoint, p3!!)) {
             currentClickedMapPoint = p2!!
             val tItem = TMapMarkerItem()
@@ -36,14 +43,15 @@ class TMapOnClickListener(private var view: MainContact.View, private var bottom
     }
 
     private fun setBottomSheetBehaviorStateCollapse() {
+        Logger.i(TAG, "setBottomSheetBehaviorStateCollapse()")
         checkPeekHeightAndSetHeight()
+        bottomSheetBehavior.isHideable = false
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-        val state = bottomSheetBehavior.state
     }
 
     private fun checkPeekHeightAndSetHeight(){
-        if (bottomSheetBehavior.peekHeight != 150) {
-            bottomSheetBehavior.peekHeight = 150
+        if (bottomSheetBehavior.peekHeight != 500) {
+            bottomSheetBehavior.peekHeight = 500
         }
     }
 
