2507910f2db1bed9891497f69490d757c65af2a2
juryun <id01096883446@gmail.com>
2019-09-17 17:34:28 +0900

    impl: cafe retrofit

--- app/src/main/java/com/gitturami/bike/model/cafe/CafeDataManager.kt
+++ app/src/main/java/com/gitturami/bike/model/cafe/CafeDataManager.kt
@@ -4,5 +4,7 @@ import android.content.Context
 import com.gitturami.bike.model.DataManager
 
 class CafeDataManager(context: Context): DataManager(context) {
+    private var api: CafeApi = retrofitConfig.getRetrofit().create(CafeApi::class.java)
 
+    val allCafe = api.getAllCafe()
 }
