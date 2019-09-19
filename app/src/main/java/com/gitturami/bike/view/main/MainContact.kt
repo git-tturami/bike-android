3805603d432e0a40c4e6011e9a28da5261a3e0ed
als95 <als950901@gmail.com>
2019-09-19 18:59:48 +0900

    fix: get station by id

--- app/src/main/java/com/gitturami/bike/view/main/MainContact.kt
+++ app/src/main/java/com/gitturami/bike/view/main/MainContact.kt
@@ -3,6 +3,7 @@ package com.gitturami.bike.view.main
 import com.gitturami.bike.model.common.pojo.DefaultItem
 import com.gitturami.bike.model.restaurant.pojo.Restaurant
 import com.gitturami.bike.model.station.pojo.Station
+import com.gitturami.bike.model.station.pojo.SummaryStation
 import com.gitturami.bike.view.main.state.State
 
 interface MainContact {
@@ -14,13 +15,14 @@ interface MainContact {
         fun showToast(title: String)
         fun setStationMarkers()
         fun setRestaurantMarkers(x: Double, y: Double, restaurant: Restaurant)
-        fun setMarker(x: Double, y: Double, station: Station)
+        fun setMarker(x: Double, y: Double, station: SummaryStation)
         fun setMarker(x: Double, y: Double, restaurant: Restaurant)
         fun onCompleteMarking()
         fun changeMarker(station: Station)
         fun setStartSearchView(text: String)
         fun setFinishSearchView(text: String)
-        fun setSelectDialogContants(station: Station)
+        fun setSelectDialogContants(id: String)
+        fun setSelectBottomSheet(station: Station)
         fun hideAllMarkers()
         fun hideCategorySheet()
         fun collapseCategorySheet()
@@ -33,6 +35,7 @@ interface MainContact {
 
     interface Presenter {
         fun takeView(view: View)
+        fun loadDetailInfoOfStation(id: String)
         fun setStationMarkers()
         fun setCafeMarkers()
         fun setRestaurantMarkers()
