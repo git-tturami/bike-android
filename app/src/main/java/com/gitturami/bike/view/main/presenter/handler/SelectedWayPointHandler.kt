3e6d4b2b44807e2544b1c96692794c7a328b2158
boohyunsik <bhs9194@nate.com>
2019-09-23 22:29:33 +0900

    impl: start & end marker impl: find including waypoint

---
+++ app/src/main/java/com/gitturami/bike/view/main/presenter/handler/SelectedWayPointHandler.kt
@@ -0,0 +1,12 @@
+package com.gitturami.bike.view.main.presenter.handler
+
+import com.gitturami.bike.logger.Logger
+import com.gitturami.bike.view.main.MainContact
+
+class SelectedWayPointHandler: StateHandler {
+    val TAG = "CategoryHandler"
+    override fun onStateChanged(view: MainContact.View) {
+        Logger.i(TAG, "onStateChanged() : Set Select Category")
+        view.hideAllMarkers()
+    }
+}
