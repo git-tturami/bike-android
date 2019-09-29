293f3f6fef18f5ad76f399c43e5d502356c6f7f2
boohyunsik <bhs9194@nate.com>
2019-09-29 15:54:32 +0900

    impl: screen shot util

---
+++ app/src/main/java/com/gitturami/bike/utils/ScreenShotUtil.kt
@@ -0,0 +1,32 @@
+package com.gitturami.bike.utils
+
+import android.graphics.Bitmap
+import android.os.Environment
+import com.gitturami.bike.logger.Logger
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+object ScreenShotUtil {
+    val TAG = "ScreenShotUtil"
+
+    fun saveBitmapToGallay(bitmap: Bitmap) {
+        val root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val dir = File("${root.absolutePath}/bike")
+        dir.mkdir()
+        val currentTime = System.currentTimeMillis()
+        val fileName = "Image-$currentTime.jpg"
+        val file = File(dir, fileName)
+        if (file.exists()) {
+            file.delete()
+        }
+        try {
+            val fileOutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fileOutputStream)
+            fileOutputStream.flush()
+            fileOutputStream.close()
+        } catch (e: IOException) {
+            Logger.e(TAG, "$e")
+        }
+    }
+}
